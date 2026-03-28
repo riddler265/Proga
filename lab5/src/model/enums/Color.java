5c50c939adf55626ffeae7c524218587f980604b
riddler265 <egoor120@gmail.com>
2026-03-28 21:13:32 +0300

    Параметры получили свои константы. Соответствующие методы get для получения параметра.

--- lab5/src/model/enums/Color.java
+++ lab5/src/model/enums/Color.java
@@ -1,12 +1,43 @@
 package model.enums;
 
+import exceptions.IncorrectInputException;
+
 /**
  * Перечисление цветов волос.
  */
 public enum Color {
-    GREEN,
-    RED,
-    BLACK,
-    YELLOW,
-    ORANGE
+    GREEN(1),
+    RED(2),
+    BLACK(3),
+    YELLOW(4),
+    ORANGE(5);
+
+    private final int id;
+    private static final String message = "\n\tGreen(1), \n\tRed(2), \n\tBlack(3), \n\tYellow(4), \n\tOrange(5)";
+
+    Color(int id) {
+        this.id = id;
+    }
+
+    public static Color getcolor(String input) throws IncorrectInputException {
+        if (input == null || input.isBlank()) {
+            throw new IncorrectInputException(message);
+        }
+
+        String trimmedInput = input.trim();
+
+        try {
+            int id = (int) Double.parseDouble(trimmedInput.replace(',', '.'));
+            for (Color color : values()) {
+                if (color.id == id) return color;
+            }
+        } catch (NumberFormatException e) {}
+
+        for (Color color : values()) {
+            if (color.name().equalsIgnoreCase(trimmedInput)) {
+                return color;
+            }
+        }
+        throw new IncorrectInputException(message);
+    }
 }
--- lab5/src/model/enums/UnitOfMeasure.java
+++ lab5/src/model/enums/UnitOfMeasure.java
@@ -1,19 +1,42 @@
 package model.enums;
 
+import exceptions.IncorrectInputException;
+
 /**
  * Перечисление единиц измерения.
  */
 public enum UnitOfMeasure {
-    METERS,
-    SQUARE_METERS,
-    LITERS,
-    GRAMS;
+    METERS(1),
+    SQUARE_METERS(2),
+    LITERS(3),
+    GRAMS(4);
 
-    /**
-     * Метод, возвращающий все константы в красивой строке.
-     * @return константы.
-     */
-    public static String units() {
-        return "Meters, square_meters, liters, grams";
+    private final int id;
+    private static final String message = "\nMeters(1)\n, Square_meters(2)\n, Liters(3)\n, grams(4)";
+
+    UnitOfMeasure(int id) {
+        this.id = id;
+    }
+
+    public static UnitOfMeasure getUnit(String input) throws IncorrectInputException {
+        if (input == null || input.isBlank()) {
+            throw new IncorrectInputException(message);
+        }
+
+        String trimmedInput = input.trim();
+
+        try {
+            int id = (int) Double.parseDouble(trimmedInput.replace(',', '.'));
+            for (UnitOfMeasure unit : values()) {
+                if (unit.id == id) return unit;
+            }
+        } catch (NumberFormatException e) {}
+
+        for (UnitOfMeasure unit : values()) {
+            if (unit.name().equalsIgnoreCase(trimmedInput)) {
+                return unit;
+            }
+        }
+        throw new IncorrectInputException(message);
     }
 }
