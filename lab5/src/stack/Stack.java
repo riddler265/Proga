823fb7ddbb62f9005a43ffd87f063d49fa0962e1
riddler265 <egoor120@gmail.com>
2026-03-29 14:55:07 +0300

    вместо логического значения выбрасывается исключение при добавлении.

--- lab5/src/stack/Stack.java
+++ lab5/src/stack/Stack.java
@@ -1,15 +1,21 @@
 package stack;
 
+import exceptions.RecursionException;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Stack {
-    private Set<File> stack = new HashSet<>();
+    private final Set<File> stack = new HashSet<>();
 
-    public boolean add(File file) throws IOException {
-        return stack.add(file.getCanonicalFile());
+    public void add(File file) throws IOException, RecursionException {
+        if (!(stack.add(file.getCanonicalFile()))) throw new RecursionException(file);
+    }
+
+    public boolean remove(File file) {
+        return stack.remove(file);
     }
 
     public void clear() {
