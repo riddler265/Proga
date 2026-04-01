d37a9ad429a348e47df1e7a077f87d95e3d98676
riddler265 <egoor120@gmail.com>
2026-04-01 22:52:29 +0300

    Remove build artifacts and javadoc from repository

--- lab5/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"collectionManager"},{"l":"commandManager"},{"l":"commands"},{"l":"coordinates"},{"l":"enums"},{"l":"exceptions"},{"l":"history"},{"l":"jsonmanager"},{"l":"person"},{"l":"product"}];updateSearchResults();
