66e4d68aa551cac972b5372c29f8de5f0534cece
riddler265 <egoor120@gmail.com>
2026-04-22 23:40:03 +0300

    Отдельный менеджер для вывода в консоль и клипания локализированных строк. Файлы .properties с нужными языками.

---
+++ lab5/src/managers/AnnounceManager.java
@@ -0,0 +1,49 @@
+package managers;
+
+import model.enums.Loc;
+
+import java.text.MessageFormat;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class AnnounceManager {
+
+    private static AnnounceManager instance;
+    private ResourceBundle bundle;
+
+    private AnnounceManager() {
+        this.bundle = ResourceBundle.getBundle("lang.messages", Loc.RUSSIAN.getLocale());
+    }
+
+    public static AnnounceManager getInstance() {
+        if (instance == null) {
+            instance = new AnnounceManager();
+        }
+        return instance;
+    }
+
+    public void setLocale(Loc locale) {
+        bundle = ResourceBundle.getBundle("lang.messages", locale.getLocale());
+    }
+
+    public String print(String key, String ... params) {
+        System.out.print(MessageFormat.format(bundle.getString(key), (Object[]) params));
+        return MessageFormat.format(bundle.getString(key), (Object[]) params);
+    }
+
+    public String println(String key, String ... params) {
+        System.out.println(MessageFormat.format(bundle.getString(key), (Object[]) params) + "\n");
+        return MessageFormat.format(bundle.getString(key), (Object[]) params);
+    }
+
+    /**
+     * <strong>C</strong>ast <strong>T</strong>o <strong>C</strong>urrent <strong>L</strong>anguage - локализует, не выводя в консоль.
+     * @param key название переменной.
+     * @param params параметры для подстановки.
+     * @return локализованное сообщение.
+     */
+    public String cTCL(String key, String ... params) {
+        return  MessageFormat.format(bundle.getString(key), (Object[]) params);
+    }
+
+}
